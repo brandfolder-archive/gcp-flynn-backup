71801a4adc1aef4f75e0c6ee69ff06365c932e93
jwaldrip <jason@waldrip.net>
2016-08-25 14:33:34 -0600

    cleanup script

--- run.sh
+++ run.sh
@@ -4,41 +4,70 @@ set -e
 # Functions
 backup-times() {
   set -e
-  echo $BACKUP_TIMES | tr ',' "\n"
+  echo $BACKUP_TIMES | tr ',' "\n" | uniq | sort
+}
+
+current-time() {
+  date +%H:%M
 }
 
 current-backup() {
   set -e
-  backup-times | uniq | grep -o "^`date +%H:%M`$"
+  backup-times | grep -o "^`current-time`$"
+}
+
+is-ten-min-mark() {
+  set -e
+  date +%M:%S | grep -oE "0:00$" > /dev/null
+}
+
+wait-for-backup() {
+  set -e
+  is-ten-min-mark || print-info "Waiting for next backup..."
+  until current-backup > /dev/null ; do
+    if is-ten-min-mark ; then print-info "Waiting for next backup..." ; fi
+    sleep 0.75
+  done
+}
+
+print-info() {
+  echo '---------------------------------'
+  if [ -n "$1" ] ; then
+    echo "$1"
+  fi
+  echo "Current time: `current-time`"
+  echo "Scheduled backup times are:"
+  backup-times | sed "s/^/• /"
 }
 
 start-backups(){
   set -e
 
-  # Sleep until next backup
-  echo "Waiting for next backup..."
-  until current-backup ; do sleep 1 ; done > /dev/null
+  wait-for-backup
 
   # Start next backup...
+  echo '---------------------------------'
   echo "Starting `current-backup` scheduled backup..."
 
   # Backup filename
-  filename="`date -Iseconds | sed -E "s/\+[0-9:]{0,}//"`-flynn-backup.tar"
+  filename="`date -Iminutes | sed -E "s/\+[0-9:]{0,}//"`-flynn-backup.tar"
 
   # Initiate Backup
-  echo "Dumping backup to local file: '/tmp/$filename'..."
-  flynn cluster backup > "/tmp/$filename"
+  echo "Dumping backup to local file: '$filename'..."
+  flynn cluster backup > "$filename"
 
   # Create bucket if it doesnt exist
   gsutil mb gs://$GOOGLE_CLOUD_STORAGE_BUCKET/ || true
 
   # Copy backup to bucket
   echo "Uploading backup to google cloud bucket: 'gs://$GOOGLE_CLOUD_STORAGE_BUCKET/$FLYNN_CLUSTER_DOMAIN'..."
-  gsutil cp "/tmp/$filename" "gs://$GOOGLE_CLOUD_STORAGE_BUCKET/$FLYNN_CLUSTER_DOMAIN"
+  gsutil cp "$filename" "gs://$GOOGLE_CLOUD_STORAGE_BUCKET/$FLYNN_CLUSTER_DOMAIN/$filename"
+  ln -s "./$filename" latest
+  gsutil cp latest "gs://$GOOGLE_CLOUD_STORAGE_BUCKET/$FLYNN_CLUSTER_DOMAIN/latest"
 
   # Clean up
-  echo "Removing local file: '/tmp/$filename'..."
-  rm -f "/tmp/$filename"
+  echo "Removing local file: '$filename'..."
+  rm -f "$filename"
 
   start-backups
 }
@@ -51,7 +80,4 @@ gcloud auth activate-service-account --key-file account.json --project `cat acco
 flynn cluster add -p $FLYNN_CERTIFICATE_PIN default $FLYNN_CLUSTER_DOMAIN $FLYNN_CONTROLLER_TOKEN
 
 # Start Backups
-echo "Starting backup job!"
-echo "Schduled backup times are:"
-backup-times
 start-backups
